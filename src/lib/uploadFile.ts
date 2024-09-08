ea4468ec7e3b158b0d06b22fc382d7e80ea212cc
mazeltovik <mazeltovik@gmail.com>
2024-09-08 13:28:17 +0300

    feat: add basic form validation

--- src/lib/uploadFile.ts
+++ src/lib/uploadFile.ts
@@ -1,12 +1,22 @@
-export default async function uploadFile(formData: FormData) {
-  'use server';
-  let response = await fetch('http://localhost:8080/upload', {
-    method: 'POST',
-    body: formData,
-  });
-  console.log(response);
-  if (response.ok) {
-    let json = await response.json();
-    console.log(json);
+'use server';
+
+const ACCEPTED_FILE_TYPES = ['image/jpeg'];
+
+export default async function uploadFile(prevState: any, formData: FormData) {
+  const file = formData.get('file');
+  if (file instanceof File) {
+    if (!ACCEPTED_FILE_TYPES.includes(file.type)) {
+      return { errMsg: 'Please choose a valid photo format' };
+    } else {
+      let response = await fetch('http://localhost:8080/upload', {
+        method: 'POST',
+        body: formData,
+      });
+      console.log(response);
+      if (response.ok) {
+        let json = await response.json();
+        console.log(json);
+      }
+    }
   }
 }
--- src/ui/component.tsx
+++ src/ui/component.tsx
@@ -1,15 +1,34 @@
+'use client';
 import uploadFile from '@/lib/uploadFile';
+import { useFormStatus, useFormState } from 'react-dom';
+import Image from 'next/image';
+
+
+const initialState = {
+  errMsg: '',
+};
+
 export default function UploadFile() {
-  // const buyProductWithId = buyProduct.bind(null)
+  const [state, formAction] = useFormState(uploadFile, initialState);
+  const { pending } = useFormStatus();
   return (
     <main className={''}>
-      <form method="POST" action={uploadFile}>
+      <form method="POST" action={formAction}>
         <div>
           <label htmlFor="file">Choose file to upload</label>
-          <input type="file" id="file" name="file" accept="image/*" />
+          <input
+            type="file"
+            id="file"
+            name="file"
+            accept="image/*"
+            onChange={() => {
+              console.log('change');
+            }}
+          />
         </div>
         <div>
-          <button>Submit</button>
+          <button disabled={pending}><Image alt='Choose photo' src='/plus-solid.svg' width={20} height={20} priority/></button>
+          <p aria-live="polite">{state?.errMsg}</p>
         </div>
       </form>
     </main>
